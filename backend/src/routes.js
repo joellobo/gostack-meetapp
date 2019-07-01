4f649b6a3c52bd99c620f24d2e1379f201781961
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-07-01 18:08:38 -0300

    feat: creating file endpoint to and saving on database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,19 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import FileController from './app/controllers/FileController'
 
-import authMiddleware from './app/middlewares/global/auth'
 import validateSessionStoreFields from './app/middlewares/session/validateStoreFields'
 import validateUserStoreFields from './app/middlewares/user/validateStoreFields'
 import validateUserUpdateFields from './app/middlewares/user/validateUpdateFields'
+import authMiddleware from './app/middlewares/global/auth'
+
+import multerConfig from './config/multer'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/users', validateUserStoreFields, UserController.store)
 routes.put(
@@ -19,5 +24,12 @@ routes.put(
 )
 
 routes.post('/sessions', validateSessionStoreFields, SessionController.store)
+
+routes.post(
+  '/files',
+  authMiddleware,
+  upload.single('file'),
+  FileController.store
+)
 
 export default routes
