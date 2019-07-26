82f0d00db679830e45266a80566d6b44216bd220
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-26 14:39:49 -0300

    Fixing button component to accept loader

--- frontend/src/components/MaButton/index.js
+++ frontend/src/components/MaButton/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { StyledButton } from './styles'
+import Loader from '../Loader'
 
-export default function MaButton({ title, ...props }) {
-  return <StyledButton {...props}>{title}</StyledButton>
+export default function MaButton({ title, isLoading, ...props }) {
+  return (
+    <StyledButton {...props}>{isLoading ? <Loader /> : title}</StyledButton>
+  )
 }
 
 MaButton.propTypes = {
   title: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool,
+}
+
+MaButton.defaultProps = {
+  isLoading: false,
 }
--- frontend/src/components/MaButton/styles.js
+++ frontend/src/components/MaButton/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 50px;
   border: none;
