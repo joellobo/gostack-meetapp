6c99165427776a13dcc6f0215c8d2857ef895236
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-26 12:55:39 -0300

    Creating container style

--- frontend/src/components/Container/styles.js
+++ frontend/src/components/Container/styles.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   min-height: 100vh;
   height: 100%;
   background: linear-gradient(#22202c, #402845);
+  justify-content: center;
 `
