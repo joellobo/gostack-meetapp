9c919c3c31b0905d0240ac5c4772f6dde0df7ad9
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-26 12:56:02 -0300

    Creating SingIn page

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-// import { Container } from './styles';
+import MaInput from '~/components/MaInput'
+import Container from '~/components/Container'
 
-import Container from '../../components/Container'
+import logo from '~/assets/logo.svg'
+
+import { MaForm } from './styles'
 
 export default function SignIn() {
   return (
     <Container>
-      <h1>SIGNING</h1>
+      <img src={logo} alt='MeetApp logo' />
+      <MaForm>
+        <MaInput name='name' placeholder='Nome Completo' />
+      </MaForm>
     </Container>
   )
 }
---
+++ frontend/src/pages/SignIn/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Form } from '@rocketseat/unform'
+
+export const MaForm = styled(Form)`
+  max-width: 350px;
+  width: 100%;
+  margin: 30px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
