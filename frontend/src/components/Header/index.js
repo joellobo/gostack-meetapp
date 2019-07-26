d17facb5ad672cb67d729a69e1c9271e770ae011
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-07-26 18:13:04 -0300

    Styling Header of the application

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import MaButton from '~/components/MaButton'
+
+import logo from '~/assets/logo.svg'
+
+import { Container, Content, Profile } from './styles'
 
 export default function Header() {
-  return <div />
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt='MeetApp logo' />
+        <Profile>
+          <img
+            src='https://api.adorable.io/avatars/50/abott@adorable.png'
+            alt='Profile'
+          />
+          <div>
+            <strong>Emilio Heinzmann</strong>
+            <Link to='/profile'>Meu perfil</Link>
+          </div>
+          <MaButton title='Sair' />
+        </Profile>
+      </Content>
+    </Container>
+  )
 }
