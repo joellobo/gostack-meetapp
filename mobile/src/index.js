da780fc88b38e5a0de1d3248a66f73efc8491f6a
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-11 14:31:22 -0300

    Finishind the config of the routes and of the redux

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import { StatusBar } from 'react-native'
+
+import './config/ReactotronConfig'
+
+import { store, persistor } from './store'
 
 import createRouter from './routes'
 
 export default function Index() {
   const Routes = createRouter(false)
 
-  return <Routes />
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle='light-content' backgroundColor='#402845' />
+        <Routes />
+      </PersistGate>
+    </Provider>
+  )
 }
