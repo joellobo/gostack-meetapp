bc310cb4845dd49ff9622cb0da1e57e22df51410
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-12 20:13:02 -0300

    Creating Dashboad page

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import Background from '~/components/Background'
+import api from '~/services/api'
 
-// import { Container } from './styles';
+import Background from '~/components/Background'
+import MeetUpCard from '~/components/MeetUpCard'
+
+import { Container, MeetUpsList, PageTitleContainer, PageTitle } from './styles'
 
 export default function Dashboard() {
-  return <Background />
+  const [meetups, setMeetups] = useState([])
+
+  useEffect(() => {
+    async function getMeetups() {
+      const response = await api.get('meetups/date', {
+        params: {
+          date: '2019-08-13T23:02:01.139Z',
+        },
+      })
+
+      setMeetups(response.data)
+    }
+
+    getMeetups()
+  }, [])
+
+  return (
+    <Background>
+      <Container>
+        <PageTitleContainer>
+          <TouchableOpacity>
+            <Icon name='keyboard-arrow-left' size={28} color='#fff' />
+          </TouchableOpacity>
+          <PageTitle>31 de Maio</PageTitle>
+          <TouchableOpacity>
+            <Icon name='keyboard-arrow-right' size={28} color='#fff' />
+          </TouchableOpacity>
+        </PageTitleContainer>
+        <MeetUpsList
+          data={meetups}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <MeetUpCard item={item} />}
+        />
+      </Container>
+    </Background>
+  )
 }
 
 Dashboard.navigationOptions = {
