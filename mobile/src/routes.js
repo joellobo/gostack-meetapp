624377e1b23a7b60b6a6b35c623595fcf971553a
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-15 21:03:28 -0300

    Adding default header to the app

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import { Image } from 'react-native'
 import {
   createAppContainer,
   createSwitchNavigator,
   createBottomTabNavigator,
+  createStackNavigator,
 } from 'react-navigation'
 
 import SignIn from './pages/SignIn'
@@ -11,6 +14,8 @@ import Dashboard from './pages/Dashboard'
 import Profile from './pages/Profile'
 import Subscriptions from './pages/Subscriptions'
 
+import logo from '~/assets/logo.png'
+
 export default (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
@@ -19,21 +24,41 @@ export default (isSigned = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator(
+        App: createStackNavigator(
           {
-            Dashboard,
-            Subscriptions,
-            Profile,
+            Index: createBottomTabNavigator(
+              {
+                Dashboard,
+                Subscriptions,
+                Profile,
+              },
+              {
+                tabBarOptions: {
+                  keyboardHidesTabBar: true,
+                  activeTintColor: '#fff',
+                  inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+                  style: {
+                    backgroundColor: '#402845',
+                    borderTopColor: 'transparent',
+                  },
+                },
+              }
+            ),
           },
           {
-            tabBarOptions: {
-              keyboardHidesTabBar: true,
-              activeTintColor: '#fff',
-              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
-              style: {
-                backgroundColor: '#402845',
-                borderTopColor: 'transparent',
+            defaultNavigationOptions: {
+              headerStyle: {
+                backgroundColor: '#22202c',
+                elevation: 0,
               },
+              headerTintColor: '#fff',
+              headerTitle: (
+                <Image
+                  source={logo}
+                  style={{ width: 35, height: 35, flex: 1 }}
+                  resizeMode='contain'
+                />
+              ),
             },
           }
         ),
