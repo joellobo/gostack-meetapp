5ab5a1cadabafc5613da4ba7518b0c87bf7d9cf8
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-21 07:29:48 -0300

    Fixing creation of db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.PASS,
   database: process.env.DB_NAME,
+  storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
     underscored: true,
