306e7d5f1c7bf83336e74323264a4c073df4b49c
Emilio Heinzmann <emiliosheinz@gmail.com>
2019-08-21 07:18:28 -0300

    fixing database config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 require('../bootstrap')
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
+  dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.PASS,
